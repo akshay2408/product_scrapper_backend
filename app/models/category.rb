5b091c41c9a5c8a35a62715b28c80ec15880728c
akshay <akshaybabar2408@gmail.com>
2022-11-10 14:29:38 +0530

    hotfix

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :products
 
   scope :with_product_detail, -> {
-    select("categories.id, categories.name, products.url as product_url, products.title as product_title")
+    select("categories.id, categories.name, products.id as product_id, products.url as product_url, products.title as product_title")
   }
 end
